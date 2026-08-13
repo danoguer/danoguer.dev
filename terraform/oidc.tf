7fcb0fa8c917fdf3e39464234a6d7d6dcd98e575
Danoguer <danoguer.dev@gmail.com>
2026-08-13 10:56:31 +0200

    fix(terraform): use dynamic tls thumbprint for github oidc provider

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -1,8 +1,11 @@
-# 1. Registrar el proveedor OIDC de GitHub en AWS IAM
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c3d101d0a520268579d4dd4944d18fa2f4f2c5f"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 data "aws_iam_policy_document" "github_actions_assume_role" {
